31fd0d75512971eec1b6652d58819e3b57b4a476
michal.kvasnicak <michal.kvasnicak@03ff94be-f950-4393-e2dd-9939f90a3152>
2010-06-19 18:44:36 +0000

    skuska dynamickeho vytvorenia edit textu... postupne sa to pokusam spravit na dynamicke dvojrozmerne pole

--- crosswordmaster/src/com/crosswordmaster/ui/anotherview.java
+++ crosswordmaster/src/com/crosswordmaster/ui/anotherview.java
@@ -3,6 +3,9 @@ package com.crosswordmaster.ui;
 import android.app.Activity;
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.util.TypedValue;
 
 import com.crosswordmaster.R;
 
@@ -18,14 +21,44 @@ public class anotherview extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.another_view);
 		
-		//pomocna premenna pre ulozenie HTML kodu
+		int x = 3;
+		int y = 3;	
+		int k,j;
+		
+		/*//pomocna premenna pre ulozenie HTML kodu
 		String html = "<html><body><h1>Anotherview</h1></body></html>";
 		
 		
 		cWebView = (WebView) findViewById(R.id.another);
         cWebView.getSettings().setJavaScriptEnabled(true);
         //cWebView.loadUrl("http://www.google.com");
-        cWebView.loadData(html, "text/html", "utf-8");
+        cWebView.loadData(html, "text/html", "utf-8");*/
+		
+		LinearLayout ll = (LinearLayout) findViewById(R.id.another);
+		
+		//EditText[][] krizovka = new EditText[x][y];
+		EditText skuska = new EditText(this);
+		skuska.setText("s");
+		skuska.setWidth(30);
+		skuska.setHeight(30);
+		skuska.setTextSize(TypedValue.COMPLEX_UNIT_SP, 10);
+		skuska.setVisibility(1);
+		skuska.setId(10);
+		ll.addView(skuska);
+		
+		//prejdeme riadky
+		/*for (k = 1; k <= x; k++) {
+			//prejdeme stlpce
+			for (j = 1; k <= y; j++) {
+				//nastavime atributy a vykreslime edittexty
+				//krizovka[k][j].setId(j*k);
+				//krizovka[k][j].setWidth(10);
+				//krizovka[k][j].setHeight(10);
+				//krizovka[k][j].layout(k * 10, j * 10, (k+1) * 10, (j+1) * 10);
+				//ll.addView(krizovka[k][j]);
+			}
+		}*/
+		
 	}
 	
 }
