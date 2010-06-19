e2df4ed7c217a14f30d9f2700537bc9f4338db51
michal.kvasnicak <michal.kvasnicak@03ff94be-f950-4393-e2dd-9939f90a3152>
2010-06-19 12:51:56 +0000

    upravena metoda onCreate zakomentovane vyplnenie layoutu main layoutom pridane zavolanie activity crosswordview, ktora sa bude starat o vyobrazenie krizovky

--- crosswordmaster/src/com/crosswordmaster/crosswordmaster.java
+++ crosswordmaster/src/com/crosswordmaster/crosswordmaster.java
@@ -1,13 +1,33 @@
 package com.crosswordmaster;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import com.crosswordmaster.ui.crosswordview;
 
 public class crosswordmaster extends Activity {
     /** Called when the activity is first created. */
+	
+	crosswordview cview;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        
+        /*
+         * Momentalne nepouzite pretoze po starte app hned tato spusti
+         * inu activity z crosswordview
+         */
+        //setContentView(R.layout.main);
+        
+        /*
+         * Vyvolanie explicitnej Activity, ktora je z vonku neviditelna
+         * Tento sposob zvysuje bezpecnost a zamedzuje zdielaniu funkcii 
+         * aplikacie s ostanymi aplikaciami tretej strany.
+         * Tato activita je definovana v AndroidManifest v zalozke 
+         * application a ako intent-filter ma uvedeny vlastny nazov a nie
+         * default akciu z Intent class.
+         */
+        startActivity(new Intent("crosswordmaster.ui.crosswordview"));
     }
 }
