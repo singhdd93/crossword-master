a8e3c7abc548c2cbbb020a38749d26ccc43c2fbe
pinkynew <pinkynew@03ff94be-f950-4393-e2dd-9939f90a3152>
2010-06-27 13:20:03 +0000

    alpha ver

---
+++ trunk/crosswordmaster/src/com/crosswordmaster/brain/Crossword.java
@@ -0,0 +1,16 @@
+package com.crosswordmaster.brain;
+
+public class Crossword {
+
+  private Field[][] fields;
+
+  public void setFields(Field[][] fields) {
+    this.fields = fields;
+  }
+
+  public Field[][] getFields() {
+    return fields;
+  }
+  
+
+}
